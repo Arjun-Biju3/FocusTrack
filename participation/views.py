a722ad7bb6d5b128de4777302bc422a73a656526
Arjun Biju <arjunbiju332@gmail.com>
2025-01-23 09:37:18 +0530

    login page added

--- participation/views.py
+++ participation/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def login_user(request):
+    return render(request,'login.html')
